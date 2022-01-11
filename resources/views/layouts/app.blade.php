2ae326360dd35f27b8f2f2d2006e44d82622637b
hr10282018 <1902422119@qq.com>
2022-01-11 04:19:59 +0000

    基础布局

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- name 和 content 属性可以一起使用，以名-值对的方式给文档提供元数据，其中 name 作为元数据的名称，content 作为元数据的值。 -->
+
+    <title>@yield('title', 'Laravel Shop') - Laravel 电商教程</title>
+    <!-- 样式 -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app" class="{{ route_class() }}-page">
+        @include('layouts._header')
+
+        <div class="container">
+            @yield('content')
+        </div>
+
+        @include('layouts._footer')
+
+    </div>
+
+    <!-- JS 脚本 -->
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
