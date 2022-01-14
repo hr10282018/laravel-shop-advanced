e2077c13e5d743a6e6241a6e64221a11b16f5b7a
hr10282018 <1902422119@qq.com>
2022-01-14 09:57:03 +0000

    管理后台-用户列表

---
+++ app/Admin/Controllers/UsersController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class UsersController extends AdminController
+{
+
+  protected $title = '用户';  // 页面标题
+
+
+  // grid()方法来决定列表页要展示哪些列，以及各个字段对应的名称
+  protected function grid()
+  {
+    $grid = new Grid(new User());
+
+    // 创建一个列名为 ID 的列，内容是用户的 id 字段
+    $grid->id('ID');
+
+    // 创建一个列名为 用户名 的列，内容是用户的 name 字段。下面的 email() 和 created_at() 同理
+    $grid->name('用户名');
+
+    $grid->email('邮箱');
+
+    // 通过display()格式化显示输出
+    // display()方法接受一个匿名函数作为参数，在展示时会把对应字段值当成参数传给匿名函数，把匿名函数的返回值作为页面输出的内容)
+    $grid->email_verified_at('已验证邮箱')->display(function ($value) {
+      return $value ? '是' : '否';
+    });
+
+    $grid->created_at('注册时间');
+
+    // 不在页面显示 `新建` 按钮，因为我们不需要在后台新建用户
+    $grid->disableCreateButton();
+    // 同时在每一行也不显示 `编辑` 按钮
+    $grid->disableActions();
+
+    $grid->tools(function ($tools) {
+      // 禁用批量删除按钮
+      $tools->batch(function ($batch) {
+        $batch->disableDelete();
+      });
+    });
+
+    return $grid;
+  }
+
+
+  // protected function detail($id)
+  // {
+  //     $show = new Show(User::findOrFail($id));
+
+  //     $show->field('id', __('Id'));
+  //     $show->field('name', __('Name'));
+  //     $show->field('email', __('Email'));
+  //     $show->field('email_verified_at', __('Email verified at'));
+  //     $show->field('password', __('Password'));
+  //     $show->field('remember_token', __('Remember token'));
+  //     $show->field('created_at', __('Created at'));
+  //     $show->field('updated_at', __('Updated at'));
+
+  //     return $show;
+  // }
+
+
+  // protected function form()
+  // {
+  //     $form = new Form(new User());
+
+  //     $form->text('name', __('Name'));
+  //     $form->email('email', __('Email'));
+  //     $form->datetime('email_verified_at', __('Email verified at'))->default(date('Y-m-d H:i:s'));
+  //     $form->password('password', __('Password'));
+  //     $form->text('remember_token', __('Remember token'));
+
+  //     return $form;
+  // }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,37 +5,28 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Encore\Admin\Traits\DefaultDatetimeFormat;// 后台-格式化日期显示
+
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
+    use DefaultDatetimeFormat;  // 后台-格式化日期显示
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
+
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
 
     // 用户-收货地址
     public function addresses()
