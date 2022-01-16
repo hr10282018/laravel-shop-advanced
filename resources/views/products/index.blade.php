af612ade6acac4048a1a64a0c6ea5908ea05de90
hr10282018 <1902422119@qq.com>
2022-01-16 10:57:37 +0000

    用户界面-商品筛选及排序

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,6 +7,29 @@
 <div class="col-lg-10 offset-lg-1">
 <div class="card" style="margin-bottom: 50px;">
   <div class="card-body">
+     <!-- 筛选组件开始 -->
+     <form action="{{ route('products.index') }}" class="search-form">
+      <div class="form-row">
+        <div class="col-md-9">
+          <div class="form-row">
+            <div class="col-auto"><input type="text" class="form-control form-control-sm" name="search" placeholder="搜索"></div>
+            <div class="col-auto"><button class="btn btn-primary btn-sm">搜索</button></div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <select name="order" class="form-control form-control-sm float-right">
+            <option value="">排序方式</option>
+            <option value="price_asc">价格从低到高</option>
+            <option value="price_desc">价格从高到低</option>
+            <option value="sold_count_desc">销量从高到低</option>
+            <option value="sold_count_asc">销量从低到高</option>
+            <option value="rating_desc">评价从高到低</option>
+            <option value="rating_asc">评价从低到高</option>
+          </select>
+        </div>
+      </div>
+    </form>
+    <!-- 筛选组件结束 -->
     <div class="row products-list">
       @foreach($products as $product)
         <div class="col-3 product-item">
@@ -24,10 +47,26 @@
         </div>
       @endforeach
     </div>
-    <div class="float-right">{{ $products->render() }}</div>  <!-- 分页组件 -->
+    <!-- 把$filters变量传给分页组件 -->
+    <div class="float-right">{{ $products->appends($filters)->render() }}</div>  <!-- 分页组件 -->
 
   </div>
 </div>
 </div>
 </div>
+@endsection
+
+@section('scriptsAfterJs')
+  <script>
+    // 把控制器传过来的$filters变量转为JSON字符串，赋值给JS的filters变量
+    var filters = {!! json_encode($filters) !!};
+    $(document).ready(function () {
+      $('.search-form input[name=search]').val(filters.search);
+      $('.search-form select[name=order]').val(filters.order);
+
+      $('.search-form select[name=order]').on('change', function() {
+        $('.search-form').submit(); // 触发提交表单事件
+      });
+    })
+  </script>
 @endsection
