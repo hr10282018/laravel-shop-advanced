1e660a256ebb95a9417dbde045ff4ed1303ec30a
hr10282018 <1902422119@qq.com>
2022-01-24 02:41:52 +0000

    用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -71,6 +71,10 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
   // 用户-申请退款
   Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
 
+  // 用户-检查优惠券
+  Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
+
+  
 });
 
 // 商品列表
